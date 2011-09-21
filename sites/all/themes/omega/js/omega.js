7ca95242d11129ed4e3eecfb00384df05133debb
Nicolas Notario <nicolasnm@gmail.com>
2011-09-21 17:39:46 +0100

    Theme added

---
+++ sites/all/themes/omega/js/omega.js
@@ -0,0 +1,24 @@
+(function ($) {
+  function populateElement(selector, defvalue) {
+      if (omega.trim($(selector).val()) == "") {
+          $(selector).val(defvalue);
+      }
+      $(selector).focus(function() {
+          if($(selector).val() == defvalue) {
+              $(selector).val("");
+          }
+      });
+      $(selector).blur(function() {
+          if(omega.trim($(selector).val()) == "") {
+              $(selector).val(defvalue);
+          }
+      });
+   }
+  
+  omega = jQuery.noConflict();
+  $(document).ready(function(){
+    // give the search box some fancy stuff
+    populateElement('#search-box input.form-text, #search-block-form input.form-text', Drupal.t(Drupal.settings.default_search_text));
+    populateElement('#search-region input.form-text', Drupal.t(Drupal.settings.default_search_text));
+  });
+})(jQuery);
